45af16c9480fd8708bd27bc780ec23fae097822e
Xin Zhao <zx0@mail.ustc.edu.cn>
2025-07-11 02:16:08 +0800

    Revert "Refactor(edit_tool): replace if-elif chain with match-case and extract command handlers"
    This reverts commit 342b4aa300d159c879f69928f86ab7bcd3b277ea.

--- trae_agent/tools/edit_tool.py
+++ trae_agent/tools/edit_tool.py
@@ -115,82 +115,63 @@ Notes for using the `str_replace` command:
         try:
             self.validate_path(command, _path)
 
-            match command:
-                case "view":
-                    return await self._handle_view_command(_path, arguments)
-                case "create":
-                    return await self._handle_create_command(_path, arguments)
-                case "str_replace":
-                    return await self._handle_str_replace_command(_path, arguments)
-                case "insert":
-                    return await self._handle_insert_command(_path, arguments)
-                case _:
+            if command == "view":
+                view_range = arguments.get("view_range", None)
+                if view_range is None:
+                    return await self.view(_path, None)
+                if not (
+                    isinstance(view_range, list) and all(isinstance(i, int) for i in view_range)
+                ):
                     return ToolExecResult(
-                        error=f"Unrecognized command {command}. The allowed commands for the {self.name} tool are: {', '.join(EditToolSubCommands)}",
+                        error="Parameter `view_range` should be a list of integers.",
                         error_code=-1,
                     )
+                view_range_int: list[int] = [i for i in view_range if isinstance(i, int)]
+                return await self.view(_path, view_range_int)
+            elif command == "create":
+                file_text = arguments.get("file_text", None)
+                if not isinstance(file_text, str):
+                    return ToolExecResult(
+                        error="Parameter `file_text` is required and must be a string for command: create",
+                        error_code=-1,
+                    )
+                self.write_file(_path, file_text)
+                return ToolExecResult(output=f"File created successfully at: {_path}")
+            elif command == "str_replace":
+                old_str = arguments.get("old_str") if "old_str" in arguments else None
+                if not isinstance(old_str, str):
+                    return ToolExecResult(
+                        error="Parameter `old_str` is required and should be a string for command: str_replace",
+                        error_code=-1,
+                    )
+                new_str = arguments.get("new_str") if "new_str" in arguments else None
+                if not (new_str is None or isinstance(new_str, str)):
+                    return ToolExecResult(
+                        error="Parameter `new_str` should be a string or null for command: str_replace",
+                        error_code=-1,
+                    )
+                return self.str_replace(_path, old_str, new_str)
+            elif command == "insert":
+                insert_line = arguments.get("insert_line") if "insert_line" in arguments else None
+                if not isinstance(insert_line, int):
+                    return ToolExecResult(
+                        error="Parameter `insert_line` is required and should be integer for command: insert",
+                        error_code=-1,
+                    )
+                new_str_to_insert = arguments.get("new_str") if "new_str" in arguments else None
+                if not isinstance(new_str_to_insert, str):
+                    return ToolExecResult(
+                        error="Parameter `new_str` is required for command: insert",
+                        error_code=-1,
+                    )
+                return self.insert(_path, insert_line, new_str_to_insert)
+            else:
+                return ToolExecResult(
+                    error=f"Unrecognized command {command}. The allowed commands for the {self.name} tool are: {', '.join(EditToolSubCommands)}",
+                    error_code=-1,
+                )
         except ToolError as e:
             return ToolExecResult(error=str(e), error_code=-1)
-
-    async def _handle_view_command(
-        self, path: Path, arguments: ToolCallArguments
-    ) -> ToolExecResult:
-        view_range = arguments.get("view_range", None)
-        if view_range is None:
-            return await self.view(path, None)
-        if not (isinstance(view_range, list) and all(isinstance(i, int) for i in view_range)):
-            return ToolExecResult(
-                error="Parameter `view_range` should be a list of integers.",
-                error_code=-1,
-            )
-        view_range_int: list[int] = [i for i in view_range if isinstance(i, int)]
-        return await self.view(path, view_range_int)
-
-    async def _handle_create_command(
-        self, path: Path, arguments: ToolCallArguments
-    ) -> ToolExecResult:
-        file_text = arguments.get("file_text", None)
-        if not isinstance(file_text, str):
-            return ToolExecResult(
-                error="Parameter `file_text` is required and must be a string for command: create",
-                error_code=-1,
-            )
-        self.write_file(path, file_text)
-        return ToolExecResult(output=f"File created successfully at: {path}")
-
-    async def _handle_str_replace_command(
-        self, path: Path, arguments: ToolCallArguments
-    ) -> ToolExecResult:
-        old_str = arguments.get("old_str") if "old_str" in arguments else None
-        if not isinstance(old_str, str):
-            return ToolExecResult(
-                error="Parameter `old_str` is required and should be a string for command: str_replace",
-                error_code=-1,
-            )
-        new_str = arguments.get("new_str") if "new_str" in arguments else None
-        if not (new_str is None or isinstance(new_str, str)):
-            return ToolExecResult(
-                error="Parameter `new_str` should be a string or null for command: str_replace",
-                error_code=-1,
-            )
-        return self.str_replace(path, old_str, new_str)
-
-    async def _handle_insert_command(
-        self, path: Path, arguments: ToolCallArguments
-    ) -> ToolExecResult:
-        insert_line = arguments.get("insert_line") if "insert_line" in arguments else None
-        if not isinstance(insert_line, int):
-            return ToolExecResult(
-                error="Parameter `insert_line` is required and should be integer for command: insert",
-                error_code=-1,
-            )
-        new_str_to_insert = arguments.get("new_str") if "new_str" in arguments else None
-        if not isinstance(new_str_to_insert, str):
-            return ToolExecResult(
-                error="Parameter `new_str` is required for command: insert",
-                error_code=-1,
-            )
-        return self.insert(path, insert_line, new_str_to_insert)
 
     def validate_path(self, command: str, path: Path):
         """Validate the path for the str_replace_editor tool."""
